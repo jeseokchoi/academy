d8b643a1a92c1bb4aa88680296257c6983c96337
jeseokchoi <105369268+jeseokchoi@users.noreply.github.com>
2022-11-13 17:06:19 +0900

    응용소프트웨어 국비수업

---
+++ day01/src/day01/Ex04.java
@@ -0,0 +1,19 @@
+package day01;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		// 논리 연산에 의한 연산 미수행
+		int n1 = -1, n2 = 0, n3 = 1, n4 = 2;
+		
+		System.out.printf("%d, %d, %d, %d\n", n1, n2, n3, n4);
+		
+		boolean flag1 = n1++ + ++n2 < 0 && n3-- * ++n4 == 0;		// 첫번째(앞의 수) 조건에서 거짓이므로 다음수를 연산하지 않고 출력한다
+		System.out.println(flag1);
+		System.out.printf("%d, %d, %d, %d\n", n1, n2, n3, n4);
+		
+		// 두 조건중 첫번째 조건만으로 전체결과가 확정된다면
+		// 두번째 조건은 연산을 수행하지 않는다
+	}
+
+}
